25e146f9f06833765b7e79d68ec0d530455474f4
Gustavo Ferroni Felipe <gustavoffelipe13012010@gmail.com>
2026-02-26 16:21:25 -0300

    Add files via upload

---
+++ Pessoa/Pessoa.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pessoa;
+
+/**
+ *
+ * @author Admin
+ */
+public class Pessoa {
+    String nome;
+    String sexo;
+    String pais;
+    
+    void pessoaAnda() {
+        System.out.println ("a pessoa está andando.");
+    }
+    void pessoaFala() {
+        System.out.println ("a pessoa está falando.");
+    }
+    void pessoaCorre() {
+        System.out.println ("a pessoa está correndo.");
+    }
+    void pessoaEstuda() {
+        System.out.println ("a pessoa está estudando.");
+    }
+    void pessoaBrinca() {
+        System.out.println ("a pessoa está brincando.");
+    }
+}
